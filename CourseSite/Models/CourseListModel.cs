f7657ff843a57938006e65aee2d5126deba6c341
jasine <jasinechen@gmail.com>
2014-12-22 03:42:49 +0800

    add some files

---
+++ CourseSite/Models/CourseListModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CourseSite.Models
+{
+
+    [DataContract(Name = "CourseInfoList")]
+    public class CourseListModel
+    {
+        //学期开始时间，修改这里变换学期以自动返回周数
+        public readonly DateTime TermStartTime = DateTime.Parse("2014-9-1");
+
+
+        [DataMember(Name = "CourseInfos")]
+        public List<CourseModel> CourseInfoList { get; set; }
+
+        [DataMember(Name = "Name")]
+        public String Name { get; set; }
+
+        [DataMember(Name = "Organization")]
+        public String Organization { get; set; }
+
+
+        private int thisWeek;
+        [DataMember (Name="ThisWeek")]
+        public int ThisWeek
+        {
+            get
+            {
+                TimeSpan ts = DateTime.Now - TermStartTime;
+                return (ts.Days)/7+1;
+            }
+            set { thisWeek = value; }
+        }
+    }
+}
