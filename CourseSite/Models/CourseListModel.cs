f0911f91a4ced13b5b560b66d5a7a2a76fe3401f
jasine <jasinechen@gmail.com>
2015-01-24 11:02:06 +0800

    添加在假期对周的判断

--- CourseSite/Models/CourseListModel.cs
+++ CourseSite/Models/CourseListModel.cs
@@ -11,7 +11,7 @@ namespace CourseSite.Models
     public class CourseListModel
     {
         //学期开始时间，修改这里变换学期以自动返回周数
-        public readonly DateTime TermStartTime = DateTime.Parse("2014-9-1");
+        public readonly DateTime TermStartTime = DateTime.Parse("2015-3-1");
 
         public int Id { get; set; }
 
@@ -32,7 +32,14 @@ namespace CourseSite.Models
             get
             {
                 TimeSpan ts = DateTime.Now - TermStartTime;
-                return (ts.Days)/7+1;
+	            if (ts.Hours < 0)
+	            {
+		            return 1;
+	            }
+	            else
+	            {
+		            return (ts.Days)/7+1;
+	            }
             }
             set { thisWeek = value; }
         }
